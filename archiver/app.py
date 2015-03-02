fb677dc77a952fcab23983622ed2b32f7a7e020d
Sean Reed <sean@cheapfit.me>
2015-03-01 22:00:14 -0500

    Add the single page basic html...need to actually style this...may just react this and make it a proper single page app. Also added another endpoint to get individual feed posts, need to fix the json serialization

--- archiver/app.py
+++ archiver/app.py
@@ -3,6 +3,7 @@ from flask.ext import restful
 from flask.ext.restful import reqparse
 
 from elasticsearch import Elasticsearch
+import rethinkdb as r
 
 app = Flask(__name__)
 api = restful.Api(app)
@@ -11,6 +12,8 @@ parser = reqparse.RequestParser()
 parser.add_argument('q', type=str, help='Query string is required!', location='args', required=True)
 
 client = Elasticsearch()
+conn = r.connect(host='localhost', port=28015)
+conn.use('facebookindex')
 
 class SearchArchive(restful.Resource):
   def get(self):
@@ -28,11 +31,20 @@ class SearchArchive(restful.Resource):
 
     return [ hit["_source"] for hit in response["hits"]["hits"]]
 
+class FeedEndpoint(restful.Resource):
+  def get(self, post_id):
+    print r.table("feed").get(post_id).run(conn)
+
 @app.route("/")
 def index():
   return render_template("index.html")
 
+@app.route("/post/<post_id>")
+def post(post_id):
+  return render_template("post.html", post_id=post_id)
+
 api.add_resource(SearchArchive, '/search')
+api.add_resource(FeedEndpoint, '/feed/<string:post_id>')
 
 if __name__ == '__main__':
   app.run(debug=True)
